24e409509bf1ca219b1384e408e1a31facc0c9b5
fulsiram <git@fulsiram.dev>
2026-04-27 22:02:54 -0400

    Configure ipv6 and allow etcd connections from cni0

--- nix/hosts/pepperoni/networking.nix
+++ nix/hosts/pepperoni/networking.nix
@@ -11,10 +11,21 @@
           prefixLength = 32;
         }
       ];
+      ipv6.addresses = [
+        {
+          address = "2a01:4f8:271:3fce::1";
+          prefixLength = 64;
+        }
+      ];
     };
 
     defaultGateway = {
       address = "162.55.238.65";
+      interface = "enp7s0";
+    };
+
+    defaultGateway6 = {
+      address = "fe80::1";
       interface = "enp7s0";
     };
 
@@ -34,4 +45,8 @@
       6443
     ];
   };
+
+  networking.firewall.extraInputRules = ''
+    iifname "cni0" tcp dport 10250 accept
+  '';
 }
