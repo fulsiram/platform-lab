a2b16351524ede62110f41e2e1bd5994a3835f4a
fulsiram <git@fulsiram.dev>
2026-05-06 19:34:00 -0400

    Update devbox ssh keys

--- nix/hosts/dev/configuration.nix
+++ nix/hosts/dev/configuration.nix
@@ -10,8 +10,8 @@
     isNormalUser = true;
     extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAso7x0jGW6tCRrV++d07ooI+lxyE1YZTAR7iYfjhHnh"
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDIACwwdSZSTjnPdksTRamfbY41sgrzKpE3IQ8A2mvN/FG7ktjNmUx1MwM4h6AMs3/6cfu2dBRwnVrsGflhXZgtcGYgvh/ffjp0zStSkRBSIXUzWd8qb7wZa+3lJFaJ1zzlQpniWxTSYlNSF0zQTBctcRdJrCVDrlnFekCMPCjXJwloY5skhIcv3iFGHTfIZLHnNlwtQw7wX0/x+bdRBtEm61MfE99jpsfe62Pkf/6fNVF5c2uy7I9pqqz+obUx9xagy6/uHrNsUiJudrhuJMPNm5QL9vIrgFQ0MDPqwUUdMZHYJ1rS7zLw0az96uBzwl10zwehWdUDRk3DtaNHIUdNMp15fb8/osykS8rBwR/U1ofnwIlct2wMI5qXA0aDU9c140AoqNlndFrZIyptUalKqeFAG2FfMO1jW0jKpgkxT4oOxRu/lwq6POU2PSShfOF104eYdNRV4TitQVLiZfImlbdnK7xzhrWUjLjMpISRG4md3IGLVzW9SR+T/s2eOTCFNv0xBAwWlSslquZBr2rGe3V8PoawdywbUs3E5/q8+G6UK7Wju6NpMSuGRdtQb0ETBAQN09uFB06TULJXvedwYuTO8KmVjpdrO4fAoXtlAp1n3ZMNWfDMfW85auMaefYpHaNuOSM9U4MGlSWsecJRCEv6c2SWmP9i7TfAGNqo0w=="
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIDHnobcDBE+4+AOdYZj2tjQcplRfazs0YAdNePw2MLkfAAAAEXNzaDpwZXJzb25hbC1hdXRo fulsiram@personal-yk-1-auth"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAjv/S+sOP+W+PUQBLpJFfczqWFfgzeWczaw0wszMrbK shadowfox@san-ti-hub"
     ];
   };
 
