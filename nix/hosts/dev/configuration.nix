6fe8d82efd6b6eb7f680c459d3792bb6b08cbc18
fulsiram <git@fulsiram.dev>
2026-05-06 22:36:07 -0400

    Enable yggdrasil on devbox

--- nix/hosts/dev/configuration.nix
+++ nix/hosts/dev/configuration.nix
@@ -6,6 +6,26 @@
 
   services.openssh.enable = true;
 
+  fileSystems."/secrets/yggdrasil-key" = {
+    fsType = "virtiofs";
+    device = "yggdrasil-secret";
+  };
+
+  services.yggdrasil = {
+    enable = true;
+    settings.Peers = [
+      # akaia.org
+      "tls://185.195.236.220:42137"
+      "tls://sto01.yggdrasil.hosted-by.skhron.eu:8884"
+    ];
+    settings.PrivateKeyPath = "/secrets/yggdrasil-key";
+  };
+
+  systemd.services.yggdrasil = {
+    requires = [ "secrets-yggdrasil-key.mount" ];
+    after = [ "secrets-yggdrasil-key.mount" ];
+  };
+
   users.users.user = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
