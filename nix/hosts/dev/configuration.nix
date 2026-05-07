ff46074b739e9b83fdf2b1feacd6899d553e4d58
fulsiram <git@fulsiram.dev>
2026-05-06 20:02:49 -0400

    Update devbox ssh keys

--- nix/hosts/dev/configuration.nix
+++ nix/hosts/dev/configuration.nix
@@ -11,6 +11,7 @@
     extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = [
       "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIDHnobcDBE+4+AOdYZj2tjQcplRfazs0YAdNePw2MLkfAAAAEXNzaDpwZXJzb25hbC1hdXRo fulsiram@personal-yk-1-auth"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGQ5mp97prL9Yvy1ZlfaEIyPaODbroGSBpzMqRANbpsv shadowfox@akaia.org"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAjv/S+sOP+W+PUQBLpJFfczqWFfgzeWczaw0wszMrbK shadowfox@san-ti-hub"
     ];
   };
