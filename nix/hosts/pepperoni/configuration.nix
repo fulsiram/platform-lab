558f32bc6507215103534af44225df3137f5431a
fulsiram <git@fulsiram.dev>
2026-06-15 00:47:55 -0400

    Configure OIDC access to k8s

--- nix/hosts/pepperoni/configuration.nix
+++ nix/hosts/pepperoni/configuration.nix
@@ -17,6 +17,30 @@
 
   services.openssh.enable = true;
 
+  environment.etc."rancher/k3s/authentication.yaml".text = ''
+    apiVersion: apiserver.config.k8s.io/v1
+    kind: AuthenticationConfiguration
+    jwt:
+    - issuer:
+        url: https://auth.salami.network/realms/members
+        audiences:
+        - kube-apiserver
+      claimMappings:
+        username:
+          claim: email
+          prefix: "oidc:"
+        groups:
+          claim: groups
+          prefix: "oidc:"
+        uid:
+          claim: sub
+      userValidationRules:
+      - expression: "!user.username.startsWith('system:')"
+        message: "username cannot use reserved system: prefix"
+      - expression: "user.groups.all(g, !g.startsWith('system:'))"
+        message: "groups cannot use reserved system: prefix"
+  '';
+
   services.k3s.enable = true;
   services.k3s.extraFlags = [
     "--cluster-init"
@@ -26,7 +50,9 @@
     "--tls-san=pepperoni.salami.network"
     "--cluster-cidr=10.42.0.0/16,fdd2:3f8b:6035:1::/56"
     "--service-cidr=10.43.0.0/16,fdd2:3f8b:6035:2::/112"
+    "--kube-apiserver-arg=authentication-config=/etc/rancher/k3s/authentication.yaml"
   ];
+
   services.k3s.disable = [
     "traefik"
     "servicelb"
